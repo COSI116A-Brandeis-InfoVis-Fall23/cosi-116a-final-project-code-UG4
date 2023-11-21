2bb4120e64b223603d9d4d4c05c713f7f1f8c4d6
Bisrat Kassie <107273623+bisrat415@users.noreply.github.com>
2023-11-21 15:02:12 -0800

    x and y axis displayed

--- js/chart.js
+++ js/chart.js
@@ -1,80 +1,195 @@
-function chart(ticker){
-    // Declare the chart dimensions and margins.
-    const width = 928;
-    const height = 600;
-    const marginTop = 20;
-    const marginRight = 30;
-    const marginBottom = 30;
-    const marginLeft = 40;
-  
-    // Declare the positional encodings.
-    const x = d3.scaleBand()
-        .domain(d3.utcDay
-            .range(ticker.at(0).Date, +ticker.at(-1).Date + 1)
-            .filter(d => d.getUTCDay() !== 0 && d.getUTCDay() !== 6))
-        .range([marginLeft, width - marginRight])
-        .padding(0.2);
-  
-    const y = d3.scaleLog()
-        .domain([d3.min(ticker, d => d.Low), d3.max(ticker, d => d.High)])
-        .rangeRound([height - marginBottom, marginTop]);
-  
-    // Create the SVG container.
-    const svg = d3.create("svg")
-        .attr("viewBox", [0, 0, width, height]);
-  
-    // Append the axes.
-    svg.append("g")
-        .attr("transform", `translate(0,${height - marginBottom})`)
-        .call(d3.axisBottom(x)
-          .tickValues(d3.utcMonday
-              .every(width > 720 ? 1 : 2)
-              .range(ticker.at(0).Date, ticker.at(-1).Date))
-          .tickFormat(d3.utcFormat("%-m/%-d")))
-        .call(g => g.select(".domain").remove());
-  
-    svg.append("g")
-        .attr("transform", `translate(${marginLeft},0)`)
-        .call(d3.axisLeft(y)
-          .tickFormat(d3.format("$~f"))
-          .tickValues(d3.scaleLinear().domain(y.domain()).ticks()))
-        .call(g => g.selectAll(".tick line").clone()
-          .attr("stroke-opacity", 0.2)
-          .attr("x2", width - marginLeft - marginRight))
-        .call(g => g.select(".domain").remove());
-  
-    // Create a group for each day of data, and append two lines to it.
-    const g = svg.append("g")
-        .attr("stroke-linecap", "round")
-        .attr("stroke", "black")
-      .selectAll("g")
-      .data(ticker)
-      .join("g")
-        .attr("transform", d => `translate(${x(d.Date)},0)`);
-  
-    g.append("line")
-        .attr("y1", d => y(d.Low))
-        .attr("y2", d => y(d.High));
-  
-    g.append("line")
-        .attr("y1", d => y(d.Open))
-        .attr("y2", d => y(d.Close))
-        .attr("stroke-width", x.bandwidth())
-        .attr("stroke", d => d.Open > d.Close ? d3.schemeSet1[0]
-            : d.Close > d.Open ? d3.schemeSet1[2]
-            : d3.schemeSet1[8]);
-  
-    // Append a title (tooltip).
-    const formatDate = d3.utcFormat("%B %-d, %Y");
-    const formatValue = d3.format(".2f");
-    const formatChange = ((f) => (y0, y1) => f((y1 - y0) / y0))(d3.format("+.2%"));
-  
-    g.append("title")
-        .text(d => `${formatDate(d.Date)}
-  Open: ${formatValue(d.Open)}
-  Close: ${formatValue(d.Close)} (${formatChange(d.Open, d.Close)})
-  Low: ${formatValue(d.Low)}
-  High: ${formatValue(d.High)}`);
-  
-    return svg.node();
+function chart() {
+  let margin = { top: 20, right: 50, bottom: 30, left: 50 },
+      width = 960 - margin.left - margin.right,
+      height = 500 - margin.top - margin.bottom,
+      xLabelText = "",
+      yLabelText = "",
+      yLabelOffsetPx = 0,
+      // xLabelText = "Time",
+      // yLabelText = "market price (open, close, change)",
+      titleText = "";
+
+
+
+  // Parse date and numbers. We assume data is in the format you provided.
+  const parseDate = d3.timeParse("%Y-%m-%d");
+
+  function chart(selector, data) {
+      data.forEach(function(d) {
+          d.Date = parseDate(d.Date);
+          d.Open = +d.Open;
+          d.High = +d.High;
+          d.Low = +d.Low;
+          d.Close = +d.Close;
+      });
+
+      // Set up scales
+      // const x = d3.scaleBand().range([0, width]).padding(0.1)
+      const x = d3.scaleBand()
+      .domain(d3.utcDay
+          .range(data.at(0).Date, +data.at(-1).Date + 1)
+          .filter(d => d.getUTCDay() !== 0 && d.getUTCDay() !== 6))
+      .range([margin.left, width - margin.right])
+      .padding(0.2);
+      const y = d3.scaleLinear().range([height, 0]);
+
+      x.domain(data.map(d => d.Date));
+      y.domain([d3.min(data, d => d.Low), d3.max(data, d => d.High)]);
+
+      // Create SVG element
+      const svg = d3.select("#vis-svg")
+          .attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+      // // Draw candlesticks
+      // svg.selectAll("rect")
+      //     .data(data)
+      //   .enter().append("rect")
+      //     .attr("x", d => x(d.Date))
+      //     .attr("width", x.bandwidth())
+      //     .attr("y", d => y(Math.max(d.Open, d.Close)))
+      //     .attr("height", d => Math.abs(y(d.Open) - y(d.Close)))
+      //     .attr("fill", d => d.Open > d.Close ? "red" : "green");
+
+      // // Draw wicks
+      // svg.selectAll("line")
+      //     .data(data)
+      //   .enter().append("line")
+      //     .attr("x1", d => x(d.Date) + x.bandwidth() / 2)
+      //     .attr("x2", d => x(d.Date) + x.bandwidth() / 2)
+      //     .attr("y1", d => y(d.High))
+      //     .attr("y2", d => y(d.Low))
+      //     .attr("stroke", "black")
+      //     .attr("stroke-width", 1);
+
+      // Add the x Axis
+      let xAxis = svg.append("g")
+          .attr("transform", "translate(0," + height + ")")
+          .call(d3.axisBottom(x).tickFormat(d3.timeFormat("%Y-%m-%d")))
+          .selectAll("text")  
+          .style("text-anchor", "end")
+          .attr("dx", "-.8em")
+          .attr("dy", ".15em")
+          .attr("transform", "rotate(-65)")
+      
+        xAxis.append("text")        
+        .attr("class", "axisLabel")
+        .attr("transform", "translate(" + (width - 50) + ",-10)")
+        .text(xLabelText);
+          
+
+      // Add the y Axis
+      let yAxis = svg.append("g")
+        .call(d3.axisLeft(y))
+      .append("text")
+        .attr("class", "axisLabel")
+        .attr("transform", "translate(" + yLabelOffsetPx + ", -12)")
+        .text(yLabelText);
+      // let yAxis = svg.append("g")
+      //     .call(d3.axisLeft(y))
+
+      chart.xLabel = function (_) {
+            if (!arguments.length) return xLabelText;
+            xLabelText = _;
+            return chart;
+          };
+           
+      chart.yLabel = function (_) {
+            if (!arguments.length) return yLabelText;
+            yLabelText = _;
+            return chart;
+          };  
+      
   }
+
+  return chart;
+}
+
+// Usage example
+// let myChart = candlestickChart();
+// d3.json("path/to/your/data.json").then(data => {
+//     myChart("#yourSvgContainer", data);
+// });
+
+
+
+// // Assuming D3.js is already included in your environment
+
+// function chart() {
+//   let margin = {top: 20, right: 20, bottom: 30, left: 50},
+//       width = 960 - margin.left - margin.right,
+//       height = 500 - margin.top - margin.bottom;
+
+//   // Parse date and numbers. We assume data is in the format you provided.
+//   const parseDate = d3.timeParse("%Y-%m-%d");
+
+//   function chart(selector, data) {
+//       data.forEach(function(d) {
+//           d.Date = parseDate(d.Date);
+//           d.Open = +d.Open;
+//           d.High = +d.High;
+//           d.Low = +d.Low;
+//           d.Close = +d.Close;
+//       });
+
+//       // Set up scales
+//       const x = d3.scaleBand().range([0, width]).padding(0.1),
+//             y = d3.scaleLinear().range([height, 0]);
+
+//       x.domain(data.map(d => d.Date));
+//       y.domain([d3.min(data, d => d.Low), d3.max(data, d => d.High)]);
+
+//       // Create SVG element
+//       const svg = d3.select("#vis-svg")
+//           .attr("width", width + margin.left + margin.right)
+//           .attr("height", height + margin.top + margin.bottom)
+//         .append("g")
+//           .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+//       // Draw candlesticks
+//       svg.selectAll(".candlestick")
+//           .data(data)
+//         .enter().append("rect")
+//           .attr("x", d => x(d.Date))
+//           .attr("y", d => y(Math.max(d.Open, d.Close)))
+//           .attr("height", d => Math.abs(y(d.Open) - y(d.Close)))
+//           .attr("width", x.bandwidth())
+//           .attr("fill", d => d.Open > d.Close ? "red" : "green");
+
+//       // Draw wicks
+//       svg.selectAll(".wick")
+//           .data(data)
+//         .enter().append("line")
+//           .attr("x1", d => x(d.Date) + x.bandwidth() / 2)
+//           .attr("x2", d => x(d.Date) + x.bandwidth() / 2)
+//           .attr("y1", d => y(d.High))
+//           .attr("y2", d => y(d.Low))
+//           .attr("stroke", "black")
+//           .attr("stroke-width", 1);
+
+//       // Add the x Axis
+//       svg.append("g")
+//           .attr("transform", "translate(0," + height + ")")
+//           .call(d3.axisBottom(x).tickFormat(d3.timeFormat("%Y-%m-%d")))
+//           .selectAll("text")  
+//           .style("text-anchor", "end")
+//           .attr("dx", "-.8em")
+//           .attr("dy", ".15em")
+//           .attr("transform", "rotate(-65)");
+
+//       // Add the y Axis
+//       svg.append("g")
+//           .call(d3.axisLeft(y));
+//   }
+
+//   return chart;
+// }
+
+// // You would then call this function with the selector of your SVG container and the data
+// // For example:
+// // let myChart = candlestickChart();
+// // d3.json("path/to/your/data.json").then(data => {
+// //     myChart("#yourSvgContainer", data);
+// // });
--- js/visualization.js
+++ js/visualization.js
@@ -2,13 +2,50 @@
 // variables and prevent 
 ((() => {
 
-  d3.json("data/BTC17_21.json", (data) => {
+  d3.json("data/NASDAQ17_21.json", (data) => {
 
     const dispatchString = "selectionUpdated";
+
+    let myChart = chart();
+    myChart("#chart", data);
+
 
     let tableData = table()
       .selectionDispatcher(d3.dispatch(dispatchString))
       ("#table", data);
 
   });
+   
+
+
+
+
+
+  // d3.json("data/BTC17_21.json", (data) => {
+
+  //   const dispatchString = "selectionUpdated";
+  //   data.forEach(d => {
+  //     d.Date = new Date(d.Date);
+  //     d.Open = +d.Open;
+  //     d.High = +d.High;
+  //     d.Low = +d.Low;
+  //     d.Close = +d.Close;
+  //     d['Adj Close'] = +d['Adj Close'];
+  //     d.Volume = +d.Volume;
+  //   });
+  
+  //   // Now you can use this data with your chart
+  //   const myChart = chart()
+  //     .width(500)
+  //     .height(300)
+  //     .ticker(data);
+  
+  //   // Append the chart to an SVG element
+  //   const svg = d3.select('#chart').append('svg');
+  //   myChart(svg);
+  // }).catch(error => {
+  //   console.error('Error loading or parsing data:', error);
+  // });
+
+
 })());
