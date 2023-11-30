94823d7033f09f9004ed52e34910bc1d131cace3
AngelaFlew <angelafliu2023@gmail.com>
2023-11-30 11:39:28 -0500

    dropdown + brushing and linking

--- js/table.js
+++ js/table.js
@@ -1,53 +1,66 @@
 /* global D3 */
 
 function table() {
+  let ourBrush = null,
+    selectableElements = d3.select(null),
+    dispatcher;
+  function chart(selector, data) {
+    let table = d3
+      .select(selector)
+      .selectAll("table")
+      .data([null])
+      .join("table")
+      .classed("my-table", true);
 
-    let ourBrush = null,
-      selectableElements = d3.select(null),
-      dispatcher;
-    function chart(selector, data) {
-        let table = d3.select(selector)
-          .append("table")
-            .classed("my-table", true);
-    
-        let tableHeaders = Object.keys(data[0]);
-    
-        let tr = table.append('thead').append('tr')
-        tr.selectAll('th').data(tableHeaders).enter().append('th').text((d) => d)
-    
-        var tbody = table.append('tbody')
-        var rows = tbody.selectAll('tr')
-        .data(data)
-        .enter()
-        .append('tr')
-        var cells = rows.selectAll('td')
-            .data((row) => { 
-                return tableHeaders.map((column) => { 
-                let value = row[column];
-                if (value instanceof Date) {
-                    value = value.toLocaleDateString('en-US', { year: 'numeric', month: 'short', day: '2-digit' });
-                }
-                return { column: column, value: value }; 
-                })
-            })
-            .enter()
-            .append('td')
-            .html((d) => d.value)
-        return chart;
-      };
+    let tableHeaders = Object.keys(data[0]);
 
-      chart.selectionDispatcher = function (_) {
-        if (!arguments.length) return dispatcher;
-        dispatcher = _;
-        return chart;
-      };
-    
-      chart.updateSelection = function (selectedData) {
-        if (!arguments.length) return;
-    
-        d3.selectAll('tr').classed("selected", d => {
-          return selectedData.includes(d)
+    let tr = table
+      .selectAll("thead")
+      .data([null])
+      .join("thead")
+      .selectAll("tr")
+      .data([null])
+      .join("tr");
+    tr.selectAll("th")
+      .data(tableHeaders)
+      .enter()
+      .append("th")
+      .text((d) => d);
+
+    var tbody = table.selectAll("tbody").data([null]).join("tbody");
+    var rows = tbody.selectAll("tr").data(data).join("tr");
+    var cells = rows
+      .selectAll("td")
+      .data((row) => {
+        return tableHeaders.map((column) => {
+          let value = row[column];
+          if (value instanceof Date) {
+            value = value.toLocaleDateString("en-US", {
+              year: "numeric",
+              month: "short",
+              day: "2-digit",
+            });
+          }
+          return { column: column, value: value };
         });
-      };
+      })
+      .join("td")
+      .html((d) => d.value);
     return chart;
+  }
+
+  chart.selectionDispatcher = function (_) {
+    if (!arguments.length) return dispatcher;
+    dispatcher = _;
+    return chart;
+  };
+
+  chart.updateSelection = function (selectedData) {
+    if (!arguments.length) return;
+
+    d3.selectAll("tr").classed("selected", (d) => {
+      return selectedData.includes(d);
+    });
+  };
+  return chart;
 }
--- js/visualization.js
+++ js/visualization.js
@@ -1,21 +1,48 @@
-// Immediately Invoked Function Expression to limit access to our 
-// variables and prevent 
-((() => {
+// Immediately Invoked Function Expression to limit access to our
+// variables and prevent
+const drawChartAndTable = async (dataPath) => {
+  const data = await d3.csv(`data/${dataPath}`);
 
-  d3.json("data/temp.json", (data) => {
+  let parseDate;
+  if (dataPath === "S_P500.csv") {
+    parseDate = d3.timeParse("%m/%d/%y");
+  } else if (dataPath === "Dow_Jones.csv") {
+    parseDate = d3.timeParse("%m/%d/%Y");
+  } else {
+    parseDate = d3.timeParse("%Y-%m-%d");
+  }
 
-    const dispatchString = "selectionUpdated";
-
-    let myChart = chart()
-      .xLabel("Date")
-      .yLabel("Market Price")
-      .yLabelOffset(0)
-      .selectionDispatcher(d3.dispatch(dispatchString))
-      ("#chart", data);
-
-    let tableData = table()
-      .selectionDispatcher(d3.dispatch(dispatchString))
-      ("#table", data);
-
+  data.forEach(function (d) {
+    d.Date = parseDate(d.Date);
+    d.Open = +d.Open;
+    d.High = +d.High;
+    d.Low = +d.Low;
+    d.Close = +d.Close;
   });
-})());
+
+  const filteredData = data
+    .filter((d) => d.Date > d3.timeParse("%Y-%m-%d")("2020-10-31"))
+    .sort((a, b) => d3.ascending(a.Date, b.Date));
+  const dispatchString = "selectionUpdated";
+  let myChart = chart()
+    .xLabel("Date")
+    .yLabel("Market Price")
+    .yLabelOffset(0)
+    .selectionDispatcher(d3.dispatch(dispatchString))("#chart", filteredData);
+
+  let tableData = table().selectionDispatcher(d3.dispatch(dispatchString))(
+    "#table",
+    filteredData
+  );
+};
+
+const datasetSelect = document.getElementById("dataSelect");
+datasetSelect.value = "BTC17_21.csv";
+
+drawChartAndTable("BTC17_21.csv");
+
+datasetSelect.addEventListener("change", function () {
+  var selectedFile = this.value;
+  // Function to update the chart
+  drawChartAndTable(selectedFile);
+});
